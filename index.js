13bf45e67289408dfb0c20d8ef5efd87826b9232
riquelmedylan <riquelmedylan@hotmail.com>
2022-12-17 19:38:32 -0300

    fix

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const sidebar = document.querySelector(".container");
+const container = document.querySelector(".container");
 const input = document.querySelector("input");
 const icon = document.querySelector(".icon-search");
 
 
-sidebar.addEventListener("click", (e) =>{
-    sidebar.classList.add("extend")
+icon.addEventListener("click", (e) =>{
+    if(container.classList.contains("extend")){
+        input.classList.remove("isActive")
+        icon.classList.remove("move")    
+        container.classList.remove("extend")
+        return
+    }
+    container.classList.add("extend")
     input.classList.add("isActive")
     icon.classList.add("move")
     input.focus()
 })
+
